51f52ea755c7410a7df2aa26ba7d79e4b45f4430
Luis Mendoza <>
2023-10-12 18:00:54 -0500

    PokeDex
    Consume Una API

---
+++ AprendiendoBlazor4Proyectos/BlazorPokedex/BlazorPokedex.Models/Sprite.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPokedex.Models
+{
+    public class Sprite
+    {
+        [JsonProperty("front_default")]
+        public string Front { get; set; }
+
+        [JsonProperty("back_default")]
+        public string Back { get; set; }
+    }
+}
---
+++ AprendiendoBlazor4Proyectos/BlazorPokedex/BlazorPokedex.Services/IPokeApiClient.cs
@@ -0,0 +1,15 @@
+﻿using BlazorPokedex.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPokedex.Services
+{
+    public interface IPokeApiClient
+    {
+        Task<ResultObject> GetAllPokemons(PaginationParameters parameters);
+        Task<Pokemon> GetPokemon(string name);
+    }
+}
