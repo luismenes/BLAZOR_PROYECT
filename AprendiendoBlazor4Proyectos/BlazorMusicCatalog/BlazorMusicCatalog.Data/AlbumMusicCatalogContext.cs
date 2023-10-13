385fca3cdc1c6f46b0e0227e31e36ff23aad9fb3
Luis Mendoza <>
2023-10-12 22:34:37 -0500

    MusicCatalog
    proyecto_ Blazor_SERVER

---
+++ AprendiendoBlazor4Proyectos/BlazorMusicCatalog/BlazorMusicCatalog.Data/AlbumMusicCatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using BlazorMusicCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMusicCatalog.Data
+{
+    public class AlbumMusicCatalogContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=-OS\\MSSQLSERVEROS;Initial Catalog=AlbumMusicCatalog;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+    }
+}
---
+++ AprendiendoBlazor4Proyectos/BlazorMusicCatalog/BlazorMusicCatalog.Data/Services/AlbumService.cs
@@ -0,0 +1,53 @@
+﻿using BlazorMusicCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMusicCatalog.Data.Services
+{
+    public class AlbumService : IAlbumService
+    {
+        private readonly AlbumMusicCatalogContext _context;
+        public AlbumService(AlbumMusicCatalogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> DeleteAlbum(Album album)
+        {
+            _context.Albums.Remove(album);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<Album> GetAlbumDetails(int id)
+        {
+            return await _context.Albums
+                          .Where(s => s.Id == id)
+                          .Include(s => s.Songs)
+                          .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Album>> GetAlbums()
+        {
+            return await _context.Albums.ToListAsync();
+        }
+
+        public async Task<bool> InsertAlbum(Album album)
+        {
+            _context.Add(album);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateAlbum(Album album)
+        {
+            _context.Update(album);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
